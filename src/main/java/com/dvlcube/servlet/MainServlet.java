d274162f6467055274c54263b32bee98f5199718
Ulisses Lima <ulisses@dvlcube.com>
2012-08-07 20:35:12 -0300

    Podia dar commit no publish...

---
+++ src/main/java/com/dvlcube/servlet/MainServlet.java
@@ -0,0 +1,29 @@
+package com.dvlcube.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(description = "Main Servlet", urlPatterns = { "/main" })
+public class MainServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("test", "OK");
+		RequestDispatcher index = request.getRequestDispatcher("index.jsp");
+		index.forward(request, response);
+	}
+}
